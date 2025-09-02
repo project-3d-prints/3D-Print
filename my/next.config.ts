21ba4ddcb25ad212b43c9e3091423914835a92fe
Kukari <alenamolodcova13@gmail.com>
2025-09-02 10:04:14 +0300

    исправлены ошибки и предупреждения

--- my/next.config.ts
+++ my/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig = {};
 module.exports = {
   reactStrictMode: true,
   swcMinify: true, // Оптимизация с SWC
-  experimental: {
-    optimizeFonts: true,
-  },
 };
 
 export default nextConfig;
